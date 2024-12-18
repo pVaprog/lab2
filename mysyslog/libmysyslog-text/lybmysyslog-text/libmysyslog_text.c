c1b20c1c5fd17a0cd5f46b0e7923e6664e8594d2
pVaprog <pva082005@gmail.com>
2024-12-19 00:21:40 +0300

    Удаление лишних файлов

--- mysyslog/libmysyslog-text/lybmysyslog-text/libmysyslog_text.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <time.h>
-#include "libmysyslog_text.h"
-
-void log_text(const char* msg, int level, const char* process) {
-	time_t now;
-	time(&now);
-	printf("%ld %d %s %s\n", now, level, process, msg);
-}
